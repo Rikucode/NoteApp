62ca48faba6593d5c5eb7ad3629d77c5167667ec
Rikucode <pavlyu4enko.a@gmail.com>
2022-12-24 18:43:08 +1000

    0.0.1

--- app/src/main/java/com/rikugou/noteapp/MainActivity.kt
+++ app/src/main/java/com/rikugou/noteapp/MainActivity.kt
@@ -2,14 +2,29 @@ package com.rikugou.noteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.rikugou.noteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.addButton.setOnClickListener() {
+            NewTaskSheet().show(supportFragmentManager, "newTaskTag")
+        }
+
+        taskViewModel.name.observe(this) {
+            binding.TaskTitle.text = String.format("Task Title: %s", it)
+        }
+
+        taskViewModel.desc.observe(this) {
+            binding.TaskDescription.text = String.format("Task Description: %s", it)
+        }
     }
 }
